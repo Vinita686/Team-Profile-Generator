734deea05962b9519c7ad6f04f51fe52341da512
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-06 17:03:35 +0000

    added function to add an intern to the team

--- starter/index.js
+++ starter/index.js
@@ -18,95 +18,143 @@ const employeeArray = [];
 
 // Create a function to gather information to pass to Engineer class
 function addEngineer() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter the Name of Engineer.",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "Enter ID of Engineer.",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Enter email of Engineer.",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Enter GitHub username of Engineer.",
-            name: "github"
-        }
-    ]).then(response => {
-        const newEngineer = new Engineer(response.name, response.id, response.email, response.github)
-        employeeArray.push(newEngineer)
-        createTeam();
-    })
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter the Name of Engineer.",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Enter ID of Engineer.",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter email of Engineer.",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Enter GitHub username of Engineer.",
+        name: "github",
+      },
+    ])
+    .then((response) => {
+      const newEngineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      employeeArray.push(newEngineer);
+      createTeam();
+    });
 }
 
-// Function to provide menu options to user to add to team.
-function createTeam() {
-    inquirer.prompt([
-        {
-      type: "list",
-      message: "Please choose from the below menu Options to make your Team.",
-      choices: ["Add an Engineer", "Add an Intern", "Finish building the Team"],
-      name: "options",
-    }
-    ]).then(response => {
-            switch(response.options) {
-            case "Add an Engineer":
-            addEngineer()
-            break;
-            case "Add an Intern":
-                // addIntern()
-                break;
-                // default: teamBuilder()
-        }
-    })
-}
-
-// Function to gather info and pass to Manager class. 
-function createManager() {
-
+// Function to gather info and pass to Intern class
+function addIntern() {
   inquirer.prompt([
     {
       type: "input",
-      message: "Please provide Team Managers's Name.",
+      message: "Enter Name of the Intern.",
       name: "name",
     },
     {
       type: "input",
-      message: "Team Manager's Employee Id?",
+      message: "Enter ID of the Intern.",
       name: "id",
     },
     {
       type: "input",
-      message: "Team Manager's Email Address?",
+      message: "Enter Email of the Intern.",
       name: "email",
     },
     {
-      type: "input",
-      message: "Team Manager's Office number?",
-      name: "number",
-    },
-    
+        type: "input",
+      message: "Enter School of the Intern.",
+      name: "school",
+    }
   ]).then(response => {
-    console.log(response)
-    const newManager = new Manager(response.name, response.id, response.email, response.number)
-    employeeArray.push(newManager)
-    console.log(employeeArray)
-    createTeam();
-  });
+    const newIntern = new Intern(response.name, response.id, response.email, response.school)
+    employeeArray.push(newIntern)
+    createTeam()
+  })
 }
 
-function teamBuilder () {
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR)
-    }
-    fs.writeFileSync(outputPath, render(employeeArray), 'utf-8')
+// Function to provide menu options to user to add employee to team.
+function createTeam() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Please choose from the below menu Options to make your Team.",
+        choices: [
+          "Add an Engineer",
+          "Add an Intern",
+          "Finish building the Team",
+        ],
+        name: "options",
+      },
+    ])
+    .then((response) => {
+      switch (response.options) {
+        case "Add an Engineer":
+          addEngineer();
+          break;
+        case "Add an Intern":
+          addIntern()
+          break;
+        // default: teamBuilder()
+      }
+    });
+}
+
+// Function to gather info and pass to Manager class.
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please provide Team Managers's Name.",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Team Manager's Employee Id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Team Manager's Email Address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "Team Manager's Office number?",
+        name: "number",
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      const newManager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.number
+      );
+      employeeArray.push(newManager);
+      console.log(employeeArray);
+      createTeam();
+    });
+}
+
+function teamBuilder() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(employeeArray), "utf-8");
 }
 
 createManager();
