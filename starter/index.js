69b229b8daab00838a075e16eb0b1b2cdfa83dfc
Vinita Yadav <yadav.vinita@gmail.com>
2023-03-06 12:46:09 +0000

    added function  to create manager using Manager class

--- starter/index.js
+++ starter/index.js
@@ -11,33 +11,71 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 const { type } = require("os");
 
+const employeeArray = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-inquirer.prompt = ([
-    {
-        type: "input",
-        message: "Please provide Team Managers's Name.",
-        title: "name",
-    },
-    {
-        type: "input",
-        message: "Team Manager's Employee Id?",
-        title: "id",
-    },
-    {
-        type: "input",
-        message: "Team Manager's Email Address?",
-        title: "email",
-    },
-    {
-        type: "input",
-        message: "Team Manager's Office number?",
-        title: "number",
-    },
-    {
-        type: "list",
-        message: "Please choose one of the below Options.",
-        choices: ["Add an Engineer", "Add an Intern", "Finish building the Team"],
-        title: "options",
+function addEngineer() {
+
+}
+
+function createTeam() {
+    inquirer.prompt([
+        {
+      type: "list",
+      message: "Please choose one of the below Options.",
+      choices: ["Add an Engineer", "Add an Intern", "Finish building the Team"],
+      name: "options",
     }
-])
+    ]).then(response => {
+        switch(response.options) {
+            case "Add an Engineer":
+            // addEngineer()
+            break;
+            case "Add an Intern":
+                // addIntern()
+                break;
+                // default: teamBuilder()
+        }
+    })
+}
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+function createManager() {
+
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Please provide Team Managers's Name.",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Team Manager's Employee Id?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Team Manager's Email Address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Team Manager's Office number?",
+      name: "number",
+    },
+    
+  ]).then(response => {
+    console.log(response)
+    const newManager = new Manager(response.name, response.id, response.email, response.number)
+    employeeArray.push(newManager)
+    console.log(employeeArray)
+    createTeam();
+  });
+}
+
+function teamBuilder () {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(employeeArray), 'utf-8')
+}
+
+createManager();
